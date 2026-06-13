4a91e8188343d1d68b2e7debedbe3e8da7d1187a
hactuss <hactuss@proton.me>
2026-06-13 16:26:28 +0200

    front 'logo'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,22 @@
 <script>
     import "./root.scss";
+    import pixel from "$lib/assets/pixel.png";
 </script>
 
 <main>
+    <!--<div class="img">
+        <img src={pixel} alt="" draggable="false" />
+        </div>-->
     <a href="/graphicdesign">graphic designs</a>
     <a href="/portfolio">portfolio</a>
     <a href="/links">links</a>
     <a href="/splatoon-3-guide">"ability farming in splatoon 3"</a>
 </main>
+
+<style>
+    .img {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>

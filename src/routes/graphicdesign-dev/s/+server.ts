e35501a12138cc06f202b09f4f913686905b06d9
hactuss <hactuss@proton.me>
2026-06-14 03:01:41 +0200

    graphicdesign auto add image

--- src/routes/graphicdesign-dev/s/+server.ts
+++
@@ -1,6 +0,0 @@
-import { asset } from "$app/paths";
-import { read } from "$app/server";
-import a from "$lib/assets/graphicdesign/asterisk.webp";
-export let _son = "";
-
-export function GET() {}
--- src/routes/graphicdesign/+page.svelte
+++ src/routes/graphicdesign/+page.svelte
@@ -1,31 +1,20 @@
 <script>
     import "./graphicdesign.scss";
-    import introduction from "$lib/assets/graphicdesign/hactus_poster.webp";
-    import asterisk from "$lib/assets/graphicdesign/asterisk.webp";
-    import obnite from "$lib/assets/graphicdesign/Obnite.webp";
-    import coral from "$lib/assets/graphicdesign/coral.png";
-    import secret_code from "$lib/assets/graphicdesign/secretcodeofhactuss.png";
-    import gentoo_propaganda from "$lib/assets/graphicdesign/gentoo-propaganda.png";
-    import nixos_propaganda from "$lib/assets/graphicdesign/nixos-propaganda.png";
+    const imageModules = import.meta.glob("$lib/assets/graphicdesign/*", {
+        query: "?url",
+    });
+    const images = Object.entries(imageModules).map(([url]) => ({ url }));
 </script>
 
 <main>
     <header>
-        <h1>Hactus Design</h1>
+        <h1>Hactus Design - this site is unproportional</h1>
     </header>
     <main>
         <div class="gallery">
-            <img src={introduction} alt="introduction" />
-            <img src={asterisk} alt="asterisk" />
-            <img src={obnite} alt="obnite" />
-            <img src={coral} alt="coral" />
-            <img src={secret_code} alt="secret_code" />
-            <img src={gentoo_propaganda} alt="gentoo_propaganda" />
-            <img src={nixos_propaganda} alt="nixos_propaganda" />
-            <img
-                src="https://cdn.masto.host/namtaocom/media_attachments/files/116/497/739/159/923/227/original/9b9adc120fa1cb42.png"
-                alt="lost_terminal"
-            />
+            {#each images as image}
+                <img src={image.url} alt={image.url} />
+            {/each}
         </div>
     </main>
 </main>
@@ -37,10 +26,6 @@
     .gallery {
         display: flex;
         flex-wrap: wrap;
-        /*flex-direction: column;
-        width: 100%;
-
         justify-content: center;
-        align-items: center;*/
     }
 </style>

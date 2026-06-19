303b7c445c2cd894b2b69d40b3fbaa52f351b4ab
hactuss <hactuss@proton.me>
2026-06-19 18:22:11 +0200

    hatshell font page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,19 +4,9 @@
 </script>
 
 <main>
-    <!--<div class="img">
-        <img src={pixel} alt="" draggable="false" />
-        </div>-->
     <a href="/graphicdesign">graphic designs</a>
     <a href="/portfolio">portfolio</a>
     <a href="/links">links</a>
     <a href="/splatoon-3-guide">"ability farming in splatoon 3"</a>
+    <a href="/hatshell">hatshell™ font family</a>
 </main>
-
-<style>
-    .img {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-</style>

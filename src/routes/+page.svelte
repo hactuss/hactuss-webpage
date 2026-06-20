b09339673ed813393a3891f376f7dc441938e022
hactuss <hactuss@proton.me>
2026-06-21 00:09:55 +0200

    autum-leaf added + pushup guy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
     import "./root.scss";
     import pixel from "$lib/assets/pixel.png";
+    import lil_guy from "$lib/assets/ezgif.com-speed.png";
 </script>
 
 <main>
+    <img src={lil_guy} alt="lil guy" />
+    <br />
     <a href="/graphicdesign">graphic designs</a>
     <a href="/portfolio">portfolio</a>
     <a href="/links">links</a>
--- src/routes/autum-leaf/+page.svelte
+++ src/routes/autum-leaf/+page.svelte
@@ -1,5 +1,29 @@
 <script>
-    import "./../root.scss";
+    //import "./../root.scss";
+    import "./autum-leaf.scss";
+    import image from "$lib/assets/autum-leaf/autum leaf by hactuss.png";
 </script>
 
-<main>comming soon</main>
+<main>
+    <img src={image} alt="font-screenshot" />
+    <section>
+        <h1>Introducing "Autum Leaf" by hactuss</h1>
+        <p>
+            "Autum Leaf" is a futuristic, hard to read font that is a
+            combination of inspiration by the Designers Republic's wip3out font,
+            and limited knowledge of Affinity. It was initially made with the
+            intent of working with a specific non-public cypher (named
+            "09-Cypher")that is based around certain characters looking similar.
+            This is the reason why many parts in the font feel reused for many
+            characters.
+        </p>
+        <h2>09-Cypher</h2>
+        <pre>
+            | A   | B   | C   | D   | Y/4 | Z   | X   |
+            | S/5 | b/6 | 7   | 0   | 3   | 2   | 8   |
+            |     |     |     |     |     | 1   |     |
+        </pre>
+    </section>
+</main>
+
+<style></style>

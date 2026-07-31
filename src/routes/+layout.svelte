7ccf4a06625c78e3ffb10a0dc5362e3439b84f1c
hactuss <hactuss@proton.me>
2026-07-31 14:12:19 +0200

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-    import favicon from "$lib/assets/favicon.svg";
+    import favicon from "$lib/assets/favicon/fav_big_blue_whitebg.svg";
     let { children } = $props();
 </script>
 
 <svelte:head>
     <title>hactuss - graphic designer and webdev</title>
-    <link rel="icon" href={favicon} />
+<link rel="icon" href={favicon} />
 </svelte:head>
 
 {@render children()}
---
+++ src/routes/+page.ts
@@ -0,0 +1,1 @@
+//export ssg = true;
